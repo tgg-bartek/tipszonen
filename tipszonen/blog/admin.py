c62b312c9d7252f6f6ab6afe76ec4be104f9a775
bartek-huss <besterbet@gmail.com>
2013-11-13 18:35:31 +0100

    added basic blog app

---
+++ tipszonen/blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin 
+
+from .models import Post 
+
+
+class PostAdmin(admin.ModelAdmin):
+	#fields = ('published', 'title', 'slug', 'content', 'author')
+	list_display = ('published', 'title', 'updated_at')
+	list_display_links = ('title',)
+	list_editable = ('published',)
+	list_filter = ('published', 'created_at', 'author')
+	prepopulated_fields = {'slug': ('title',)}
+	search_fields = ('title', 'content')
+
+admin.site.register(Post, PostAdmin)
---
+++ tipszonen/blog/views.py
@@ -0,0 +1,21 @@
+from django.views.generic import ListView, DetailView
+
+from .models import Post 
+
+
+class PublishPostsMixin(object):
+	'Return posts ticked as published'
+	def get_queryset(self):
+		queryset = super(PublishPostsMixin, self).get_queryset()
+		return queryset.filter(published=True )
+
+
+class PostListView(PublishPostsMixin, ListView):
+	model = Post 
+	template_name = 'blog/index.html'
+	context_object_name = 'published_post_list'
+
+class PostDetailView(PublishPostsMixin, DetailView):
+	model = Post 
+	template_name = 'blog/detail.html'
+
